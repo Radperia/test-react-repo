55ca7106de4db522f62e42051d0056152b812de3
Radperia <rexalmeria826@gmail.com>
2023-05-04 16:11:40 +0900

    230504 added some components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { Home } from "./Home.jsx";
+import { Page1 } from "./Page1.jsx";
+import { Page2 } from "./Page2.jsx";
+import { Page3 } from "./Page3.jsx";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Test App
+          </Typography>
+          <Button color="inherit" component={Link} to="/Home">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/Page1">
+            Page1
+          </Button>
+          <Button color="inherit" component={Link} to="/Page2">
+            Page2
+          </Button>
+          <Button color="inherit" component={Link} to="/Page3">
+            Page3
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/Page2" element={<Page2 />} />
+        <Route path="/Page3" element={<Page3 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
