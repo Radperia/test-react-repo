db10fca3c282b9b98473cb4b6313854c35e2754c
Radperia <rexalmeria826@gmail.com>
2023-05-05 16:29:26 +0900

    230505 added page3 and builded

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,17 +1,20 @@
 import "./Page2.css";
 import { Helmet } from "react-helmet";
 import Trans from "./components/Change";
+import Layout from './components/Layout';
 
 export const Page2 = () => {
     return (
-      <div style={{ width: "500px", height: "500px", overflow: "hidden" }}>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title> Page2 title </title>
-        </Helmet>
-        <h1>Page2</h1>
-        <Trans />
-      </div>
+      <Layout>
+        <div style={{ width: "500px", height: "500px", overflow: "hidden" }}>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title> Page2 title </title>
+          </Helmet>
+          <h1>Page2</h1>
+          <Trans />
+        </div>
+      </Layout>
     );
   };
   
--- src/Page3.jsx
+++ src/Page3.jsx
@@ -1,14 +1,40 @@
-import { Helmet } from "react-helmet";
+import { useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Box = () => {
+  const ref = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [speed, setSpeed] = useState(0);
+
+  useFrame((state, delta) => {
+    if (isHovered) {
+      setSpeed(Math.min(speed + delta * 1.5, 7));
+    } else {
+      setSpeed(Math.max(speed - delta * 1.2, 0));
+    }
+    ref.current.rotation.x += speed * delta;
+    ref.current.rotation.y += speed * delta;
+  });
+
+  return (
+    <mesh
+      ref={ref}
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+    >
+      <boxBufferGeometry args={isHovered ? [2, 2, 2] : [1, 1, 1]} />
+      <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x9178e6} />
+    </mesh>
+  );
+};
 
 export const Page3 = () => {
-    return (
-      <div>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title> Page3 title </title>
-        </Helmet>
-        <h1>Page3</h1>
-      </div>
-    );
-  };
-  
+  return (
+    <Canvas dpr={2} style={{ height: "80vh" }}>
+      <color attach="background" args={[0xffffff]} />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={0.5} position={[-10, 10, 10]} />
+      <Box />
+    </Canvas>
+  );
+};
